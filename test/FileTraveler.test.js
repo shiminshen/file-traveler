2cb4d36849449f2986e1f2329351a8ba6e35d71b
smshen <thanatos1710@gmail.com>
2017-08-22 00:18:35 +0800

    add chai and test of rename and mkdir

--- test/FileTraveler.test.js
+++ test/FileTraveler.test.js
@@ -1,5 +1,13 @@
-import FileTraveler from '../src'
+import FileTraveler, {
+  listFiles,
+  listFilesSync,
+  rename,
+  renameSync,
+  mkDir,
+  mkDirSync
+} from '../src'
 import assert from 'assert'
+import { expect } from 'chai'
 
 import FixtureDir from 'fixture-dir'
 import mochaFixtureDir from 'mocha-fixture-dir'
@@ -21,20 +29,54 @@ describe('FileTraveler', () => {
     { name: 'file1.js', isDirectory: false }
   ]
 
-  describe('list files', () => {
+  // list files testing
+  describe('List files', () => {
 
     it('Sychronous listFilesSync', () => {
-      assert.deepEqual(fileList, FileTraveler.listFilesSync(tmpFilesPath));
+      let files = FileTraveler.listFilesSync(tmpFilesPath)
+      expect(files).to.deep.equal(fileList)
     });
 
     it('Asynchronous listFiles', (done) => {
       FileTraveler.listFiles(tmpFilesPath)
       .then( files => {
-        assert.deepEqual(fileList, files)
+        expect(files).to.deep.equal(fileList)
         done()
       })
       .catch(e => done(e))
     });
   });
+
+  describe('Rename files', () => {
+
+    let renamedFileList = [
+      { name: 'renamed.js', isDirectory: false },
+      { name: 'renamedDir', isDirectory: true }
+    ]
+
+    it('Synchronous rename', () => {
+
+        renameSync(`${tmpFilesPath}/directory1`, `${tmpFilesPath}/renamedDir`)
+        renameSync(`${tmpFilesPath}/file1.js`, `${tmpFilesPath}/renamed.js`)
+
+        let files = FileTraveler.listFilesSync(tmpFilesPath)
+        expect(files).to.deep.equal(renamedFileList)
+    })
+  })
+
+  describe('Make directory', () => {
+    it('Synchronous mkDir', () => {
+
+      let beforeFiles = FileTraveler.listFilesSync(tmpFilesPath)
+      expect(beforeFiles).to.be.an('array').deep.not.include({ name: 'newDir', isDirectory: true })
+      mkDirSync(`${tmpFilesPath}/newDir`)
+
+      let afterFiles = FileTraveler.listFilesSync(tmpFilesPath)
+
+      expect(afterFiles).to.be.an('array').deep.include({ name: 'newDir', isDirectory: true })
+
+    })
+
+  })
 
 });
